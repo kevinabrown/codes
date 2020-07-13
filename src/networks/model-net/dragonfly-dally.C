6fcd49930780c84727690dc03102dbcb669e40e4
Kevin A. Brown <kabrown@anl.gov>
2020-07-13 05:22:31 +0000

    DFDally: PRINT_MSG_TIMES to print message latencies
    and record and report the router queueing time for each packet.

--- src/networks/model-net/dragonfly-dally.C
+++ src/networks/model-net/dragonfly-dally.C
@@ -43,6 +43,7 @@
 // debugging parameters
 #define BW_MONITOR 1
 #define DEBUG_LP 892
+#define PRINT_MSG_TIMES 1
 #define T_ID -1
 #define TRACK -1
 #define TRACK_PKT -1
@@ -1036,6 +1037,10 @@ static long long       *total_msg_sz;
 static long long       *N_finished_msgs;
 static long long       *N_finished_chunks;
 
+#if PRINT_MSG_TIMES == 1
+FILE *rdfdally_file;
+#endif
+
 /* convert ns to seconds */
 static tw_stime ns_to_s(tw_stime ns)
 {
@@ -1938,6 +1943,10 @@ void dragonfly_dally_report_stats()
             printf("\n[QOS_Class:%d] Total packets generated %ld finished %ld Locally routed- same router %ld different-router %ld Remote (inter-group) %ld \n", 
                     i, total_gen[i], total_fin[i], total_local_packets_sr[i], total_local_packets_sg[i], total_remote_packets[i]);
         }
+#if PRINT_MSG_TIMES == 1
+        fclose(rdfdally_file);
+        system("mv /tmp/r-dfdally.out ./");
+#endif
     }
     free(avg_hops);
     free(total_finished_packets);
@@ -1953,6 +1962,7 @@ void dragonfly_dally_report_stats()
     free(total_local_packets_sr);
     free(total_local_packets_sg);
     free(total_remote_packets);
+
 
     return;
 }
@@ -2793,6 +2803,10 @@ void terminal_dally_init( terminal_state * s, tw_lp * lp )
         minimal_count = (long*)calloc(num_qos_levels, sizeof(long));
         nonmin_count = (long*)calloc(num_qos_levels, sizeof(long));
 
+#if PRINT_MSG_TIMES == 1
+        rdfdally_file = fopen("/tmp/r-dfdally.out", "w");
+        fprintf(rdfdally_file, "time destination source qosclass num.hops latency router.queue.time");
+#endif
     }
  
     s->packet_gen = (long*)calloc(num_qos_levels, sizeof(long));
@@ -3838,6 +3852,15 @@ static void packet_arrive(terminal_state * s, tw_bf * bf, terminal_dally_message
     msg->saved_rcv_time = stat->recv_time;
     stat->recv_time += (tw_now(lp) - msg->travel_start_time);
 
+#if PRINT_MSG_TIMES == 1
+    /* We get the exact vcg set on the packet in case num_qos_levels == 0 */
+    int vc_group = get_vcg_from_category(msg);
+    fprintf(rdfdally_file, "\n%lf %d %d %d %d %lf %lf", tw_now(lp), s->terminal_id,
+            codes_mapping_get_lp_relative_id(msg->sender_mn_lp,0,0), 
+            vc_group, msg->my_N_hop, (tw_now(lp) - msg->travel_start_time),
+            msg->router_stall_total_time);
+#endif
+
 #if DEBUG == 1
     if( msg->packet_ID == TRACK 
             && msg->chunk_id == num_chunks-1
@@ -4452,8 +4475,13 @@ static void router_packet_receive( router_state * s,
     terminal_dally_message_list * cur_chunk = (terminal_dally_message_list*)calloc(1, sizeof(terminal_dally_message_list));
     init_terminal_dally_message_list(cur_chunk, msg);
     
+    cur_chunk->msg.router_stall_start_time = tw_now(lp);
     if(cur_chunk->msg.last_hop == TERMINAL) // We are first router in the path
+    {
         cur_chunk->msg.path_type = MINIMAL; // Route always starts as minimal
+
+        cur_chunk->msg.router_stall_total_time = 0;
+    }
 
     Connection next_stop_conn = do_dfdally_routing(s, bf, &(cur_chunk->msg), lp, dest_router_id);
     msg->num_rngs += (cur_chunk->msg).num_rngs; //make sure we're counting the rngs called during do_dfdally_routing()
@@ -4761,6 +4789,11 @@ static void router_packet_send( router_state * s, tw_bf * bf, terminal_dally_mes
     msg->saved_available_time = s->next_output_available_time[output_port];
     s->next_output_available_time[output_port] = 
         maxd(s->next_output_available_time[output_port], tw_now(lp));
+
+    // Record how long the packet was queued before it could progress
+    cur_entry->msg.router_stall_total_time += s->next_output_available_time[output_port] - 
+                                                cur_entry->msg.router_stall_start_time;
+
     s->next_output_available_time[output_port] += injection_delay;
 
     injection_ts = s->next_output_available_time[output_port] - tw_now(lp);
